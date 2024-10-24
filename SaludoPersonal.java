facfcde1a572206f10f1f978875ae468780b8dbe
Samuel Gaviria Morales <sagamo2504@hotmail.com>
2024-10-24 11:52:27 -0500

    Ejercicios requeridos en la clase dos de Dev Senior

---
+++ SaludoPersonal.java
@@ -0,0 +1,7 @@
+public class SaludoPersonal {
+    public static void main(String[] args) {
+        
+        System.out.println("Samuel, bienvenido al curso de Programacion en Java!!!");
+        
+    }
+}
